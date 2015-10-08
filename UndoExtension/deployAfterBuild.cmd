4dd3cf8caa653ff596a7addbdb869216f7e9eae5
Josh Elster <jelster@inrule.com>
2015-10-08 12:12:07 -0500

    added script to copy extension to irAuthor folder. requires appropriate perms on folder

---
+++ UndoExtension/deployAfterBuild.cmd
@@ -0,0 +1,3 @@
+copy bin\debug\undoextension.dll "c:\program files (x86)\inrule\irauthor\extensions\undoextension.dll"
+copy bin\debug\undoextension.pdb "c:\program files (x86)\inrule\irauthor\extensions\undoextension.pdb"
+c:\program files (x86)\inrule\irauthor\irauthor.exe
