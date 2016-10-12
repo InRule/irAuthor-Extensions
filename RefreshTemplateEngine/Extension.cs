6945ea96e867d998038682fd2b0e65c2b4de8aa4
jeffreykey <jkey@inrule.com>
2016-10-12 11:00:48 -0500

    Initial commit.

---
+++ RefreshTemplateEngine/Extension.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using InRule.Authoring.Commanding;
+using InRule.Authoring.Media;
+using InRule.Authoring.Windows;
+using InRule.Common.Utilities;
+using InRule.Repository;
+
+namespace InRule.Authoring.Extensions.RefreshTemplateEngine
+{
+	public class Extension : ExtensionBase
+	{
+		private readonly VisualDelegateCommand _refreshTemplateEngineCommand;
+
+		public Extension()
+			: base("RefreshTemplateEngine", "Refreshes the template engine", new Guid("{D9DA19AA-2C3E-4510-921B-C87F4FA60BCE}"))
+		{
+			_refreshTemplateEngineCommand = new VisualDelegateCommand(RefreshTemplateEngine, "Refresh Template Engine", ImageFactory.GetImageAuthoringAssembly("/Images/Refresh32.png"), null, false);
+		}
+
+		public override void Enable()
+		{
+			var ruleAppGroup = IrAuthorShell.HomeTab.GetGroup("Rule Application");
+			ruleAppGroup.AddButton(_refreshTemplateEngineCommand);
+
+			RuleApplicationService.RuleApplicationDefChanged += WhenRuleApplicationDefChanged;
+		}
+
+		private void WhenRuleApplicationDefChanged(object sender, EventArgs<RuleApplicationDef> e)
+		{
+			_refreshTemplateEngineCommand.IsEnabled = RuleApplicationService.RuleApplicationDef != null;
+		}
+
+		private void RefreshTemplateEngine(object obj)
+		{
+			var selectedDef = SelectionManager.SelectedItem;
+			SelectionManager.SelectedItem = null;
+
+			RuleApplicationService.ResetTemplateEngine(true);
+
+			SelectionManager.SelectedItem = selectedDef;
+		}
+	}
+}
