9e976f6c8a9643de61231f51a0cf231121d93901
jeffreykey <jkey@inrule.com>
2016-10-14 22:10:03 -0500

    Initial commit.

---
+++ NavigationToolWindows/Extension.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Controls;
+using InRule.Authoring.Core.Navigation;
+using InRule.Authoring.Windows;
+using System.Linq;
+
+namespace InRule.Authoring.Extensions.NavigationToolWindows
+{
+	public class Extension : ExtensionBase
+	{
+		public static Guid ID = new Guid("{871C3491-096B-4351-9F7A-BE006CE147CE}");
+
+		public Extension()
+			: base("NavigationToolWindows", "Allows navigation panes to be converted to tool windows and vice versa", ID)
+		{}
+
+		public override void Enable()
+		{
+			var navBar = IrAuthorShell.NavigationControl as INavigationBar;
+
+			if (navBar != null)
+			{
+				var navPanes = navBar.NavigationPanes.ToList();
+
+				foreach (var navPane in navPanes)
+				{
+					var control = (Control)navPane.Content;
+					var parentControl = (ContentControl)control.Parent;
+
+					navBar.RemoveNavigationPane(navPane);
+
+					parentControl.Content = null;
+
+					var host = ServiceManager.Compose<ToolWindowHost>(control, navBar, navPane.Title, navPane.ImageSourceSmall, navPane.ImageSourceLarge);
+
+					navBar.AddNavigationPane(host, navPane.Name, navPane.Title, navPane.ImageSourceSmall, navPane.ImageSourceLarge);
+				}
+			}
+		}
+	}
+}
---
+++ NavigationToolWindows/ToolWindowHost.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using InRule.Authoring.Core.Navigation;
+using InRule.Authoring.Windows;
+
+namespace InRule.Authoring.Extensions.NavigationToolWindows
+{
+	public partial class ToolWindowHost : UserControl
+	{
+		public IIrAuthorShell IrAuthorShell { get; set; }
+
+		private readonly INavigationBar _navigationBar;
+		private readonly string _title;
+		private readonly ImageSource _imageSourceSmall;
+		private readonly ImageSource _imageSourceLarge;
+		private string _popOutToolTip = "Move into tool window.";
+		private string _popInToolTip = "Move into the navigation bar.";
+
+		public ToolWindowHost(object control, INavigationBar navigationBar, string title, ImageSource imageSourceSmall, ImageSource imageSourceLarge)
+		{
+			InitializeComponent();
+
+			content.Content = control;
+			_navigationBar = navigationBar;
+			_title = title;
+			_imageSourceSmall = imageSourceSmall;
+			_imageSourceLarge = imageSourceLarge;
+
+			button.ToolTip = _popOutToolTip;
+		}
+
+		private void WhenButtonClicked(object sender, RoutedEventArgs e)
+		{
+			if (Parent != null)
+			{
+				if (Parent.GetType().Name == "NavigationPane")
+				{
+					button.Margin = new Thickness(0, -20, 35, 0);
+					button.Content = "ß";
+					button.ToolTip = _popInToolTip;
+
+					var navigationPane = _navigationBar.GetNavigationPane(_title);
+
+					if (navigationPane != null)
+					{
+						_navigationBar.RemoveNavigationPane(navigationPane);
+
+						var control = (Control)navigationPane.Content;
+						var parentControl = (ContentControl)control.Parent;
+
+						parentControl.Content = null;
+
+						var toolWindow = IrAuthorShell.AddToolWindow(control, "toolWindow" + GetHashCode(), _title, false, _imageSourceSmall);
+						toolWindow.CanClose = false;
+						toolWindow.Dock();
+					}
+				}
+				else if (Parent.GetType().Name == "InRuleToolWindow")
+				{
+					button.Margin = new Thickness(0, -23, 0, 0);
+					button.Content = "Þ";
+					button.ToolTip = _popOutToolTip;
+
+					var toolWindow = (HeaderedContentControl)Parent;
+
+					toolWindow.Content = null;
+					toolWindow.GetType().GetMethod("Destroy", new Type[0]).Invoke(toolWindow, new object[0]);
+
+					var navigationBar = IrAuthorShell.NavigationControl as INavigationBar;
+
+					if (navigationBar != null)
+					{
+						var navigationPane = navigationBar.AddNavigationPane(this, _title + GetHashCode(), _title, _imageSourceSmall, _imageSourceLarge);
+						navigationBar.SelectedPane = navigationPane;
+					}
+				}
+			}
+		}
+	}
+}
