96b0e723f7f3044551e1a51d163b580c70dd08a4
Josh Elster <jelster@inrule.com>
2019-02-01 16:49:10 -0600

    Remove call to nuget restore in build script

--- build-all.ps1
+++ build-all.ps1
@@ -1,1 +1,1 @@
-gci **\*.sln -recurse -Exclude Master.sln  | foreach { nuget restore $_;msbuild  $_ /t:Rebuild /p:platform="any cpu" /p:Configuration="Debug" /p:OutDir=$("$(PWD)\Output\" + $_.BaseName)}
+gci **\*.sln -recurse -Exclude Master.sln  | foreach { msbuild $_ /t:Rebuild /p:platform="any cpu" /p:Configuration="Debug" /p:OutDir=$("$(PWD)\Output\" + $_.BaseName)}
