c710c9ae20460a51aeeb85948f1d6b2502921e9f
Josh Elster <jelster@inrule.com>
2019-02-01 13:21:02 -0600

    Fix-up package references to point to new target framework. Add build script

---
+++ build-all.ps1
@@ -0,0 +1,1 @@
+gci **\*.sln -recurse -Exclude Master.sln  | foreach { nuget restore $_;msbuild  $_ /t:Rebuild /p:platform="any cpu" /p:Configuration="Debug" /p:OutDir=$("$(PWD)\Output\" + $_.BaseName)}
