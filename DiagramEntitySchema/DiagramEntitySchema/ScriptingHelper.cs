cd2ef3aeacb0334f735fe3c9f019b9a3983dc5a1
Dan Gardiner <dangardiner@gmail.com>
2021-01-05 18:00:03 -0600

    Create DiagramEntitySchema extension

---
+++ DiagramEntitySchema/DiagramEntitySchema/ScriptingHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Permissions;
+using System.Runtime.InteropServices;
+using InRule.Authoring.Extensions.DiagramEntitySchema;
+
+namespace InRule.Authoring.Extensions.DiagramEntitySchema
+{
+    [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+    [ComVisible(true)]
+    public class ScriptingHelper
+    {
+        ReportWindow mExternalWPF;
+
+        public ScriptingHelper(ReportWindow w)
+        {
+            this.mExternalWPF = w;
+        }
+
+        public void SelectElementInIrAuthor(string jsscript)
+        {
+        }
+
+        public void ResizeControl(int width, int height)
+        {
+            this.mExternalWPF.Resize(height, width, true);
+        }
+    }
+
+}
---
+++ DiagramEntitySchema/README.md
@@ -0,0 +1,4 @@
+# Entity Schema Diagram Tool for IrAuthor
+Loads an interactive graph of the entity schema and relationships between entities in the Rule App.  Collection-type relationships are indicated by a thicker line, and Temporary fields/Collections are indicated by a lighter-colored line.
+
+## Version History
